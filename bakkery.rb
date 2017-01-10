88a854b5c9a19ab4223d9216daa109c00ebdd8fe
mippet <pimluitjes@gmail.com>
2017-01-10 15:16:19 +0100

    added empty shoppingcard and products

---
+++ bakkery.rb
@@ -0,0 +1,14 @@
+@shopping_cart = []
+
+@products = [
+  {article: "loaf of bread" , number: 231, price: 4 },
+  {article: "half of a loaf of bread", number: 222, price: 2},
+  {article: "whole wheat bread", number: 322 , price: 4},
+  {article: "half whole wheat bread", number: 432, price: 6},
+  {article: "baguette", number: 542, price: 3},
+  {article: "bastard bread", number: 341, price: 5},
+  {article: "flat bread", number: 232, price: 5},
+  {article: "round bread", number: 341, price: 3},
+  {article: "square bread", number: 532, price: 8},
+
+]
